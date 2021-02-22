d4718590b8748ed5e9c4df62b5eb08594080fab0
Dubz <wchadun@gmail.com>
2021-02-22 17:19:25 +0400

    Add files via upload

---
+++ cryptotracker_coinbase.py
@@ -0,0 +1,22 @@
+import datetime
+from coinbase.wallet.client import Client
+
+user = Client('uY7uxzizqDLfH5s7', 'WBSa5pAzABTqpXn28m1OzWQt1I1HCiuH')
+input_text = "What cryptocurrency do you want to check on?\nEnter the in this format 'cryptocurrency-currency': "
+while True:
+    currency_pair = input(input_text)
+    try:
+        price = user.get_spot_price(currency_pair=currency_pair.upper())
+
+        amount = price["amount"]
+        base = price["base"]
+        currency = price["currency"]
+
+        print(f"The price of {base} in {currency} is: {amount} on {datetime.date.today()}. ")
+        break
+
+    except:
+        input_text = "Please enter the correct currency pair i.e. 'TheCryptoCurrency-TheCurrencyYouWantToConvertItTO': "
+
+
+
